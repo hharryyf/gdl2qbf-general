015189322883639a45a21716161eb805be601e65
hharryyf <harryheyifan99@gmail.com>
2025-12-02 22:36:51 +1100

    update

--- extg2qbf_turn.py
+++ extg2qbf_turn.py
@@ -37,7 +37,7 @@ def log_action_encoding(inputfile, player, f):
     print(f'ldom({player}, 1..{tol}).', file=f)
     print('% log-encoding', file=f)
 
-    print(f"{{moveL({player}, M, T) : ldom({player}, M)}} :- mtdom(T).",file=f)
+    print(f"{{moveL({player}, M, T) : ldom({player}, M)}} :- mtdom(T), _player_turn({player},T).",file=f)
     
     j = 0
     for i in range(0, 1 << tol):
@@ -59,106 +59,200 @@ def log_action_encoding(inputfile, player, f):
     print(file=f)
 
 
-def build_quantifier(current, gamefile, quantifier):
-    cmd = f'clingo --output=smodels log-encoding.lp {gamefile} > smodels.txt'
+def build_quantifier(current, gamefile, quantifier, turn):
+    cmd = f'clingo --output=smodels log-encoding.lp {gamefile} {turn} > smodels.txt'
     os.system(f"bash -c '{cmd}'")
 
     outputfile = open(file=quantifier, mode='w')
 
-    state = 0
-    universal = {}
-    exist = {}
-    exist_o = {}
-
-    
+    state, mxv = 0, 0
+    edge = set()
+    vertex, universal, exist = {}, {}, {}
+    otherexist = {}
+    choices = set()
+    #edgespos = set()
     with open('smodels.txt') as f:
         for line in f:
             line = line.strip()
             if line == '0':
                 state += 1
                 continue
-            if state == 1:
+            if state == 0:
+                line = list(map(int, line.split()))
+                # normal rule
+                # head number_of_lit number_of_neg_lit [negative lit] [positive lit]
+                if line[0] == 1:
+                    head = line[1]
+                    for i in range(4, len(line)):
+                        if line[i] == 1:
+                            print('Unexpected Error')
+                            exit(1)
+                        edge.add((line[i], head))
+                        # add checks for the desired structure
+                        #if i >= line[3] + 4:
+                        #    edgespos.add((line[i], head, 1))
+                        #else:
+                        #    edgespos.add((line[i], head, -1))
+
+                # head number_of_lit number_of_neg_lit bound [negative lit] [positive lit]
+                elif line[0] == 2:
+                    head = line[1]
+                    for i in range(5, len(line)):
+                        edge.add((line[i], head))
+                        #if i >= line[3] + 5:
+                        #    edgespos.add((line[i], head, 2))
+                        #else:
+                        #    edgespos.add((line[i], head, -2))
+
+                # number_of_head [head] number_of_lit number_of_neg_lit [negative lit] [positive lit]
+                elif line[0] == 3:
+                    head_num = line[1]
+                    head = []
+                    for i in range(2, head_num + 2):
+                        head.append(line[i])
+                        choices.add(line[i])
+                    # this part can be optimized
+                    for i in range(head_num + 4, len(line)):
+                        for h in head:
+                            edge.add((line[i], h))
+                            #if i >= line[4] + 4 + head_num:
+                            #    edgespos.add((line[i], h, 3))
+                            #else:
+                            #    edgespos.add((line[i], h, -3))
+
+                else:
+                    print('Cannot handle rule of type 4+ in Clingo!')
+                    exit(1)
+            elif state == 1:
                 line = line.split()
-                _, atom = int(line[0]), line[1]
-                if atom[:5] != 'does(' and atom[:6] != 'moveL(':
-                    continue
+                vid, atom = int(line[0]), line[1]
+                mxv = max(mxv, vid)
                 newl = atom.replace('(', ',').replace(')',',').split(',')
+                ours = False
+                others = False
+                if atom[:5] == 'does(' or atom[:6] == 'moveL(':
+                    if vid in choices:
+                        choices.remove(vid)
+                for cu in current:
+                    lencu = len(f'does({cu},')
+                    if atom[:lencu] == f'does({cu},':
+                        ours = True
+                        break
+
+                if ours == False and atom[:5] == 'does(':
+                    others = True
+
+                if others == False and ours == False and atom[:6] != 'moveL(':
+                    vertex[vid] = (atom, -1)
+                    continue
+                    
+
                 lv = -1
                 for i in range(len(newl) - 1, -1, -1):
                     if len(newl[i]) and newl[i] != '\n':
                         lv = int(newl[i])
                         break
-                if lv != -1:
-                    ours = False
-                    for cu in current:
-                        lencu = len(f'does({cu},')
-                        if atom[:lencu] == f'does({cu},':
-                            ours = True
-                            break
-
+                if others == True and lv != -1:
+                    vertex[vid] = (atom, -1)
+                    if lv not in otherexist:
+                        otherexist[lv] = []
+                    otherexist[lv].append(vid)
+                
+                if lv != -1 and others == False:
+                    vertex[vid] = (atom, lv)
                     if ours == True:
                         if lv in exist:
-                            exist[lv].append(atom)
+                            exist[lv].append(vid)
                         else:
                             exist[lv] = []
-                            exist[lv].append(atom)
+                            exist[lv].append(vid)
                     elif atom[:6] == 'moveL(':
                         if lv in universal:
-                            universal[lv].append(atom)
+                            universal[lv].append(vid)
                         else:
                             universal[lv] = []
-                            universal[lv].append(atom)
-                    else:
-                        if lv in exist_o:
-                            exist_o[lv].append(atom)
-                        else:
-                            exist_o[lv] = []
-                            exist_o[lv].append(atom)
+                            universal[lv].append(vid)
 
+    if len(choices) != 0:
+        print('Our method is inapplicable! Clingo is rewriting choice rules! ', choices)
+        exit(1)
+    
+    #print(edgespos)
 
-    univ = list(universal.items())
-    univ.sort()
-    exist = list(exist.items())
-    exist.sort()
-    exist_o = list(exist_o.items())
-    exist_o.sort()
+    for lv in universal.keys():
+        if lv in otherexist:
+            for u in universal[lv]:
+                for e in otherexist[lv]:
+                    #edge.add((u,e))
+                    if vertex[u][0][6:].split(',')[0] == vertex[e][0][5:].split(',')[0]:
+                        edge.add((u,e))
+                        #print(vertex[u][0][6:].split(',')[0], vertex[e][0][5:].split(',')[0])
 
-    i = 1
-    for tup in univ:
-        for it in tup[1]:
-            print(f'_forall({i*2}, {it}).', file=outputfile)
-        i += 1
+    for e in edge:
+        mxv = max(mxv, max(e[0], e[1]))
 
-    for tup in exist:
-        lv = 2 * len(univ) + 1
-        ok = False
-        for i in range(0, len(univ), 1):
-            if tup[0] <= univ[i][0]:
-                ok = True
-                for it in tup[1]:
-                    print(f'_exists({2 * i + 1}, {it}).', file=outputfile)
-                break
+    univ_out = []
+    exist_in = []
+    for univ in universal.items():
+        lv = univ[0]
+        univ_out.append((lv, mxv + 1))
+        for uv in univ[1]:
+            edge.add((uv, mxv + 1))
+        mxv += 1
 
-        if ok == False:
-            for it in tup[1]:
-                print(f'_exists({lv}, {it}).', file=outputfile)
+    for exi in exist.items():
+        lv = exi[0]
+        exist_in.append((lv, mxv + 1))
+        for ex in exi[1]:
+            edge.add((mxv + 1, ex))
+        mxv += 1
 
-    for tup in exist_o:
-        lv = 1
-        ok = False
-        for i in range(len(univ) - 1, -1, -1):
-            if tup[0] >= univ[i][0]:
-                ok = True
-                lv = 2 * i + 3
-                for it in tup[1]:
-                    print(f'_exists({lv}, {it}).', file=outputfile)
-                break
+    univ_out.sort()
+    exist_in.sort()
 
-        if ok == False:
-            for it in tup[1]:
-                print(f'_exists({lv}, {it}).', file=outputfile)                
+    luniv, lexist = len(univ_out), len(exist_in)
+    i, j = luniv - 1, lexist - 1
+    while i >= 0:
+        while j >= 0 and exist_in[j][0] > univ_out[i][0]:
+            edge.add((univ_out[i][1], exist_in[j][1]))
+            j -= 1
+        i -= 1
 
-        
+    graph = []
+    visited = []
+    for i in range(0, mxv + 1):
+        graph.append([])
+        visited.append(-1)
+
+    for e in edge:
+        graph[e[0]].append(e[1])
+
+    def bfs(v, uv, depth):
+        q = queue.Queue()
+        q.put(v)
+        while q.empty() == False:
+            curr = q.get()
+            if visited[curr] == 1:
+                continue
+            if uv != curr and curr in vertex:
+                print(f'_exists({depth},{vertex[curr][0]}).', file=outputfile)
+            visited[curr] = 1
+
+            for nxt in graph[curr]:
+                if visited[nxt] != 1:
+                    q.put(nxt)
+
+    for i in range(luniv - 1, -1, -1):
+        for uv in universal[univ_out[i][0]]:
+            print(f'_forall({i * 2 + 2},{vertex[uv][0]}).', file=outputfile)
+        for uv in universal[univ_out[i][0]]:
+            if visited[uv] != 1:
+                bfs(uv, uv, i * 2 + 3)
+
+    for i in range(1, mxv + 1):
+        if i in vertex and visited[i] != 1:
+            print(f'_exists(1,{vertex[i][0]}).', file=outputfile)
+
     outputfile.close()
 
 
@@ -237,7 +331,7 @@ def get_other(inputfile, current):
     return list(res)
 
 
-def gdl2qbf(current, other, gamefile, preprocess, outfile):
+def gdl2qbf(current, other, gamefile, preprocess, outfile, turn):
     logfile = 'log-encoding.lp'
     f = open(logfile, 'w')
 
@@ -275,8 +369,8 @@ def gdl2qbf(current, other, gamefile, preprocess, outfile):
     for o in other:
         log_action_encoding(gamefile, o, f)
     f.close()
-    build_quantifier(current, gamefile, 'quantifier.lp')
-    cmd = f'clingo --output=smodels {gamefile}  log-encoding.lp quantifier.lp | python qasp2qbf.py --no-warning  | lp2normal2 | lp2acyc | lp2sat | python qasp2qbf.py --cnf2qdimacs > {outfile}'
+    build_quantifier(current, gamefile, 'quantifier.lp', turn)
+    cmd = f'clingo --output=smodels {gamefile}  log-encoding.lp quantifier.lp {turn} | python qasp2qbf.py | lp2normal2 | lp2acyc | lp2sat | python qasp2qbf.py --cnf2qdimacs > {outfile}'
     os.system(f"bash -c '{cmd}'")
 
     if preprocess == True:
@@ -290,8 +384,8 @@ def gdl2qbf(current, other, gamefile, preprocess, outfile):
         print(f'Bloqqer finishes in {round(end - start, 2)}s')
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python extg2qbf.py [config.json]")
+    if len(sys.argv) != 3:
+        print("Usage: python extg2qbf.py [config.json] [path to turn.lp]")
         exit(1)
     fp = open(sys.argv[1])
     js = json.load(fp)
@@ -301,7 +395,8 @@ if __name__ == "__main__":
     preprocessor = js['preprocessor']
     output = js['output']
     fp.close()
-    
+    turn = sys.argv[2]
+
     if len(other) == 0:
         # single-player game
         cmd = f'clingo {gamefile} action-generator-single.lp'
@@ -309,5 +404,5 @@ if __name__ == "__main__":
         exit(0)
 
     # competitive goal
-    gdl2qbf(current, other, gamefile, preprocessor, output)
+    gdl2qbf(current, other, gamefile, preprocessor, output, turn)
     
